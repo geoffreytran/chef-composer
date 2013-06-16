f552f6c0efe96ec1a156f348114a6c2ef56ec1b9
Geoffrey Tran <geoffrey.tran@gmail.com>
2013-06-15 20:25:55 -0500

    Update default.rb

--- resources/default.rb
+++ resources/default.rb
@@ -2,3 +2,4 @@ actions :deploy, :install
 
 # Where to deploy composer.phar
 attribute :deploy_to, :kind_of => String, :name_attribute => true
+attribute :composer_home, :kind_of => String, default => ""
